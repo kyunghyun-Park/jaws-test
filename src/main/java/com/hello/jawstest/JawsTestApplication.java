97d1e5d181984ff03bd908bd33dcf112f4cdf5d7
kyunghyun-park <qqqq_o@naver.com>
2024-01-28 17:41:19 +0900

    initial commit

---
+++ src/main/java/com/hello/jawstest/JawsTestApplication.java
@@ -0,0 +1,13 @@
+package com.hello.jawstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JawsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JawsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hello/jawstest/JawsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.jawstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JawsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
